a1e86d5073bf7569f10a8199dee6a6d08baa0a85
ivanadnin <ivandanin01@gmail.com>
2021-06-23 18:13:19 +0300

    jdbc lab

---
+++ Database Access with JDBC/src/Diablo.java
@@ -0,0 +1,10 @@
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Diablo {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        Properties properties = new Properties();
+    }
+}
---
+++ Database Access with JDBC/src/Main.java
@@ -0,0 +1,37 @@
+import java.sql.*;
+import java.util.Properties;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+
+        try {
+            Properties properties = new Properties();
+            properties.setProperty("user", "root");
+            properties.setProperty("password", "root");
+
+            Connection connection = DriverManager.getConnection
+                    ("jdbc:mysql://localhost:3306/soft_uni", properties);
+
+            PreparedStatement statement = connection.prepareStatement("SELECT `first_name`, `last_name` " +
+                    " FROM `employees` " +
+                    "WHERE `salary` > ?");
+
+            System.out.println("Insert salary");
+            String salary = scanner.nextLine();
+            statement.setDouble(1, Double.parseDouble(salary));
+
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString("first_name") + " "
+                        + resultSet.getString("last_name"));
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
