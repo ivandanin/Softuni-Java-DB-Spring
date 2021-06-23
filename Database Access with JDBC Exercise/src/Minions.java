02947f823140b3e018be28e71dee184442a842b7
ivanadnin <ivandanin01@gmail.com>
2021-06-23 18:45:31 +0300

    jdbc lab

--- Database Access with JDBC Exercise/src/Minions.java
+++ Database Access with JDBC Exercise/src/Minions.java
@@ -1,2 +1,10 @@
-package PACKAGE_NAME;public class Minions {
+import java.util.Properties;
+
+public class Minions {
+    public static void main(String[] args) {
+
+        Properties properties = new Properties();
+        properties.setProperty("user", "root");
+        properties.setProperty("password", "root"); /// TODO: 6/23/2021 Enter pass 
+    }
 }
